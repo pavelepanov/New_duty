0aab1935f9ac6371a0ba14fb8abd71ba817a96c4
Vismut <pavel.epanovm@gmail.com>
2023-05-29 11:29:53 +0500

    Add utils for xlsx

--- src/xlsx/router.py
+++ src/xlsx/router.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
-from datetime import datetime
-import xlsxwriter
 
-from student.repository import RepositoryGrade, RepositoryStudent
-from student.router import get_student_info_by_id, get_grade_info_by_id
-from defection.router import get_defection_no_card_all, get_defection_appearance_all, get_defection_being_late_all
+from xlsx.utils import get_table_no_card, get_table_appearance, get_table_being_late
 
 router = APIRouter(
     prefix="/xlsx",
@@ -12,97 +8,9 @@ router = APIRouter(
 )
 
 
-@router.get("/no_card")
-async def get_table_no_card():
-    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
-    # открываем новый файл на запись
-    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_no_card.xlsx')
-    # создаем там "лист"
-    worksheet = workbook.add_worksheet()
-    # Шапка, где классы
-    for col in range(1, 12):
-        worksheet.write(0, col - 1, f"{col} класс")
-
-    student_id = await get_defection_no_card_all()
-    student_id_all = {}
-    for id in student_id:
-        if student_id[id] != 0:
-            student_id_all[id] = student_id[id]
-
-    for student_id in student_id_all:
-        quantity = student_id_all[student_id]
-        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
-        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
-        grade_student = grade_student[0].grade
-        worksheet.write(dict_grade[grade_student] + 1,
-                        grade_student - 1,
-                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
-        dict_grade[grade_student] += 1
-        print(dict_grade)
-    # сохраняем и закрываем
-    workbook.close()
-
-
-@router.get("/being_late")
-async def get_table_no_card():
-    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
-    # открываем новый файл на запись
-    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_being_late.xlsx')
-    # создаем там "лист"
-    worksheet = workbook.add_worksheet()
-    # Шапка, где классы
-    for col in range(1, 12):
-        worksheet.write(0, col - 1, f"{col} класс")
-
-    student_id = await get_defection_being_late_all()
-    student_id_all = {}
-
-    for id in student_id:
-        if student_id[id] != 0:
-            student_id_all[id] = student_id[id]
-
-    for student_id in student_id_all:
-        quantity = student_id_all[student_id]
-        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
-        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
-        grade_student = grade_student[0].grade
-        worksheet.write(dict_grade[grade_student] + 1,
-                        grade_student - 1,
-                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
-        dict_grade[grade_student] += 1
-        print(dict_grade)
-    # сохраняем и закрываем
-    workbook.close()
-
-
-@router.get("/appearance")
-async def get_table_no_card():
-    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
-    # открываем новый файл на запись
-    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_appearance.xlsx')
-    # создаем там "лист"
-    worksheet = workbook.add_worksheet()
-    # Шапка, где классы
-    for col in range(1, 12):
-        worksheet.write(0, col - 1, f"{col} класс")
-
-    student_id = await get_defection_appearance_all()
-
-    student_id_all = {}
-
-    for id in student_id:
-        if student_id[id] != 0:
-            student_id_all[id] = student_id[id]
-
-    for student_id in student_id_all:
-        quantity = student_id_all[student_id]
-        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
-        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
-        grade_student = grade_student[0].grade
-        worksheet.write(dict_grade[grade_student] + 1,
-                        grade_student - 1,
-                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
-        dict_grade[grade_student] += 1
-        print(dict_grade)
-    # сохраняем и закрываем
-    workbook.close()
+@router.get("/xlsx")
+async def get_xlsx():
+    no_card = await get_table_no_card()
+    being_late = await get_table_being_late()
+    appearance = await get_table_appearance()
+    return no_card, being_late, appearance
---
+++ src/xlsx/utils.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+
+import xlsxwriter
+
+from defection.router import get_defection_no_card_all, get_defection_being_late_all, get_defection_appearance_all
+from student.repository import RepositoryStudent, RepositoryGrade
+
+
+async def get_table_no_card():
+    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
+    # открываем новый файл на запись
+    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_no_card.xlsx')
+    # создаем там "лист"
+    worksheet = workbook.add_worksheet()
+    # Шапка, где классы
+    for col in range(1, 12):
+        worksheet.write(0, col - 1, f"{col} класс")
+
+    student_id = await get_defection_no_card_all()
+    student_id_all = {}
+    for id in student_id:
+        if student_id[id] != 0:
+            student_id_all[id] = student_id[id]
+
+    for student_id in student_id_all:
+        quantity = student_id_all[student_id]
+        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
+        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
+        grade_student = grade_student[0].grade
+        worksheet.write(dict_grade[grade_student] + 1,
+                        grade_student - 1,
+                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
+        dict_grade[grade_student] += 1
+        print(dict_grade)
+    # сохраняем и закрываем
+    workbook.close()
+
+
+async def get_table_being_late():
+    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
+    # открываем новый файл на запись
+    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_being_late.xlsx')
+    # создаем там "лист"
+    worksheet = workbook.add_worksheet()
+    # Шапка, где классы
+    for col in range(1, 12):
+        worksheet.write(0, col - 1, f"{col} класс")
+
+    student_id = await get_defection_being_late_all()
+    student_id_all = {}
+
+    for id in student_id:
+        if student_id[id] != 0:
+            student_id_all[id] = student_id[id]
+
+    for student_id in student_id_all:
+        quantity = student_id_all[student_id]
+        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
+        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
+        grade_student = grade_student[0].grade
+        worksheet.write(dict_grade[grade_student] + 1,
+                        grade_student - 1,
+                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
+        dict_grade[grade_student] += 1
+        print(dict_grade)
+    # сохраняем и закрываем
+    workbook.close()
+
+
+async def get_table_appearance():
+    dict_grade = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0}
+    # открываем новый файл на запись
+    workbook = xlsxwriter.Workbook(f'students{datetime.now()}_appearance.xlsx')
+    # создаем там "лист"
+    worksheet = workbook.add_worksheet()
+    # Шапка, где классы
+    for col in range(1, 12):
+        worksheet.write(0, col - 1, f"{col} класс")
+
+    student_id = await get_defection_appearance_all()
+
+    student_id_all = {}
+
+    for id in student_id:
+        if student_id[id] != 0:
+            student_id_all[id] = student_id[id]
+
+    for student_id in student_id_all:
+        quantity = student_id_all[student_id]
+        info_about_student = await RepositoryStudent.get_student_info_by_id(student_id)
+        grade_student = await RepositoryGrade.get_info_by_group_id(info_about_student.group_id)
+        grade_student = grade_student[0].grade
+        worksheet.write(dict_grade[grade_student] + 1,
+                        grade_student - 1,
+                        "%s %s %d" % (info_about_student.name, info_about_student.surname, quantity))
+        dict_grade[grade_student] += 1
+        print(dict_grade)
+    # сохраняем и закрываем
+    workbook.close()
